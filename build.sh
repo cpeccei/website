84a777f10c476869ce7016f7c32aff5402f9792f
Christian Peccei <christian.peccei@gmail.com>
2020-03-29 14:17:19 -0700

    Move build system to pandoc

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+build () {
+  cur_dir=$(dirname "$1")
+  # pandoc -s -o "$cur_dir/index.html" --template=template.html "$1"
+  if [[ "$(uname)" == MSYS* ]]; then
+      css=//base.css
+  else
+      css=/base.css
+  fi
+  pandoc -s -o "$cur_dir/index.html" --highlight-style tango --css "$css" "$1"
+}
+
+build content/body.md
+for body in content/*/body.md; do
+  build "$body"
+done
---
+++ content/body.md
@@ -0,0 +1,11 @@
+---
+title: Welcome
+---
+
+Hello world
+
+[Solving Doublets (Word Ladders) in Python](/doublets-in-python/)
+
+[Visualizing the Similarity of Musical Artists](/musicmap/)
+
+[Solving Doublets (Word Ladders) in Mathematica](/doublets/)
