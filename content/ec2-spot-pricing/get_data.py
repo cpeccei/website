b22963e1bb38d87bb3f202e166fb2bd44aaf0f15
Christian Peccei <christian.peccei@gmail.com>
2020-10-02 09:33:00 -0700

    Add ec2 spot pricing

---
+++ content/ec2-spot-pricing/get_data.py
@@ -0,0 +1,56 @@
+import boto3
+import time
+import json
+
+client = boto3.client('ec2', region_name='us-west-1')
+regions = [x["RegionName"] for x in client.describe_regions()["Regions"]]
+
+stats = []
+
+for region in regions:
+    client = boto3.client('ec2', region_name=region)
+
+    # Can also be achieved by calling client.describe_instance_types() and
+    # manually paging using NextToken, but this is nicer
+    paginator = client.get_paginator('describe_instance_types')
+    instance_types = {entry['InstanceType']: entry
+        for page in paginator.paginate() for entry in page['InstanceTypes']}
+
+    paginator = client.get_paginator('describe_spot_price_history')
+    now = time.time()
+    response_iterator = paginator.paginate(
+        StartTime=now,
+        ProductDescriptions=['Linux/UNIX'])
+    spot_prices = [spot_price for page in response_iterator
+        for spot_price in page['SpotPriceHistory']]
+
+    for row in spot_prices:
+        data = instance_types[row['InstanceType']]
+        s = {
+            'instance_type': row['InstanceType'],
+            'dollars_per_hour': float(row['SpotPrice']),
+            'region': region,
+            'availability_zone': row['AvailabilityZone'],
+            'current_generation': data['CurrentGeneration'],
+            'architecture': ('arm64' if 'arm64' in
+                data['ProcessorInfo']['SupportedArchitectures'] else 'x86_64'),
+            'vcpus': data['VCpuInfo']['DefaultVCpus'],
+            'memory_gib': data['MemoryInfo']['SizeInMiB'] / 1024.
+        }
+        s['memory_gib_per_vcpu'] = s['memory_gib'] / s['vcpus']
+        s['memory_gib_per_dollar'] = s['memory_gib'] / s['dollars_per_hour']
+        s['vcpus_per_dollar'] = s['vcpus'] / s['dollars_per_hour']
+        s['power'] = (0.036 * s['vcpus'] + 0.0034 * s['memory_gib'] +
+            (0.2 if 'd' in s['instance_type'][2:].split('.')[0] else 0.0) +
+            (0.3 if 'n' in s['instance_type'][2:].split('.')[0] else 0.0))
+        for k, v in s.items():
+             if isinstance(v, float):
+                s[k] = round(v, 4)
+        stats.append(s)
+    print('Finished', region)
+
+stats.sort(key=lambda x: (x['dollars_per_hour'], -x['power']))
+json_data = json.dumps(stats)
+with open('stats.json', 'w') as f:
+    f.write(json_data)
+
---
+++ content/ec2-spot-pricing/local.js
@@ -0,0 +1,149 @@
+var request = new XMLHttpRequest();
+request.open('GET', 'stats.json', false);  // `false` makes the request synchronous
+request.send(null);
+data = JSON.parse(request.responseText)
+
+var fr = document.getElementById("filter-region");
+var regions = {};
+for (i = 0; i < data.length; i++) {
+  regions[data[i].region] = true;
+}
+regions = Object.keys(regions);
+regions.sort();
+regions.forEach(function (item, index) {
+  var option = document.createElement("option");
+  option.text = item;
+  fr.add(option);
+});
+fr.value = "us-west-2";
+
+function reset() {
+  document.getElementById("filter-memory").value = 0;
+  document.getElementById("filter-vcpus").value = 0;
+  document.getElementById("filter-mpv").value = 0;
+  document.getElementById("filter-architecture").value = "any";
+  document.getElementById("filter-type").value = "";
+  document.getElementById("filter-region").value = "us-west-2";
+  document.getElementById("filter-currentgen").checked = false;
+  updateChart();
+}
+
+var seriesData;
+function getSeriesData() {
+  memory = document.getElementById("filter-memory").value
+  vcpus = document.getElementById("filter-vcpus").value
+  mpv = document.getElementById("filter-mpv").value
+  insttype = document.getElementById("filter-type").value
+  architecture = document.getElementById("filter-architecture").value
+  region = document.getElementById("filter-region").value
+  currentgen = document.getElementById("filter-currentgen").checked
+  seriesData = {categories: [], price: [], power: []};
+  power = [];
+  const regex = RegExp(insttype);
+  for (i = 0; i < data.length; i++) {
+    if (seriesData.categories.length == 20) {
+      break;
+    }
+    if (data[i].memory_gib < memory) {
+      continue;
+    };
+    if (data[i].vcpus < vcpus) {
+      continue;
+    };
+    if (data[i].memory_gib_per_vcpu < mpv) {
+      continue;
+    };
+    if (!regex.test(data[i].instance_type)) {
+      continue;
+    };
+    if (currentgen && !data[i].current_generation) {
+      continue;
+    };
+    if (region != data[i].region) {
+      continue;
+    };
+    if (architecture != "any" && architecture != data[i].architecture) {
+      continue;
+    };
+    label = '<b>' + data[i].instance_type + '</b><br>' +
+      data[i].availability_zone + '<br>' +
+      data[i].memory_gib + ' GiB, ' + data[i].vcpus + ' vCPUs<br>' +
+      '$' + data[i].dollars_per_hour + ' / hour<br>' +
+      '$' + (data[i].dollars_per_hour  * 24).toFixed(4) + ' / day<br>';
+    seriesData.categories.push(data[i].instance_type);
+    seriesData.price.push({name: label, y: data[i].dollars_per_hour,
+      color: null});
+    // seriesData.price.push(data[i].dollars_per_hour);
+    seriesData.power.push(data[i].power);
+  }
+  minPower = Math.min(...seriesData.power);
+  maxPower = Math.max(...seriesData.power);
+  for (i = 0; i < seriesData.power.length; i++) {
+    // c = (seriesData.power[i] - minPower) / (maxPower - minPower) * 255;
+    seriesData.price[i].color = "rgb(" +
+      interp(seriesData.power[i], minPower, maxPower, 199, 65)   + ", " +
+      interp(seriesData.power[i], minPower, maxPower, 233, 171) + ", " +
+      interp(seriesData.power[i], minPower, maxPower, 192, 93)  + ")"
+  }
+}
+
+function interp(i, ib, ie, ob, oe) {
+  if (ib == ie) {return 0.5 * (oe - ob) + ob};
+  return Math.round((i - ib) / (ie - ib) * (oe - ob) + ob)
+}
+
+function updateChart() {
+  getSeriesData();
+  myChart.series[0].setData(seriesData.price);
+  myChart.xAxis[0].setCategories(seriesData.categories);
+}
+
+Highcharts.setOptions({
+    chart: {
+        style: {
+            fontFamily: 'sans-serif'
+        }
+    }
+});
+
+document.getElementById("filter-memory").addEventListener("keyup", updateChart);
+document.getElementById("filter-vcpus").addEventListener("keyup", updateChart);
+document.getElementById("filter-mpv").addEventListener("keyup", updateChart);
+document.getElementById("filter-type").addEventListener("keyup", updateChart);
+document.getElementById("filter-architecture").addEventListener("change", updateChart);
+document.getElementById("filter-region").addEventListener("change", updateChart);
+document.getElementById("filter-currentgen").addEventListener("change", updateChart);
+
+getSeriesData();
+
+var myChart = Highcharts.chart({
+  chart: {
+    type: 'bar',
+    renderTo: 'container'
+  },
+  legend: {
+      enabled: false
+  },
+  title: {
+    text: undefined
+  },
+  xAxis: {
+    title: {
+      text: 'Instance Type'
+    },
+    categories: seriesData.categories
+  },
+  yAxis: {
+    title: {
+      text: '$ per hour'
+    }
+  },
+  tooltip: {
+    formatter: function() {
+      return this.point.name;
+    }
+  },
+  series: [{
+      data: seriesData.price
+    }]
+});
