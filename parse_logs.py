0ddc07022984d85f666a5350710b5e56a7bb9de3
Christian Peccei <christian.peccei@gmail.com>
2020-10-11 22:32:40 -0700

    Logging utilities

--- parse_logs.py
+++ parse_logs.py
@@ -6,11 +6,20 @@ import time
 
 log_folder = sys.argv[1]
 access_logs = pathlib.Path(log_folder).glob('access*')
+print('time_utc', 'request_type', 'url', 'response', sep='\t')
 for access_log in access_logs:
     opener = gzip.open if access_log.suffix == '.gz' else open
     with opener(access_log, 'rt') as f:
         for line in f:
-            time_str = re.search(r'\[.*?\]', line).group(0)
-            time_struct = time.strptime(time_str, '[%d/%b/%Y:%H:%M:%S %z]')
-            fmt_time = time.strftime('%Y-%m-%d %H:%M:%S', time_struct)
-            print(fmt_time)
+            m = re.search(r'\[.*?\]', line)
+            if m:
+                time_str = m.group(0)
+                time_struct = time.strptime(time_str, '[%d/%b/%Y:%H:%M:%S %z]')
+                time_utc = time.strftime('%Y-%m-%d %H:%M:%S', time_struct)
+            else:
+                time_utc = ''
+            m = re.search('"(GET|POST) (.*?) .*?" (\\d+) ', line)
+            request_type = m.group(1) if m else ''
+            url = m.group(2) if m else ''
+            response = m.group(3) if m else ''
+            print(time_utc, request_type, url, response, sep='\t')
