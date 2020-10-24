8d2ea27e89691ce0123c3a1b76120d09f19585c5
Christian Peccei <christian.peccei@gmail.com>
2020-10-24 12:07:02 -0700

    Fix update script

--- content/ec2-spot-pricing/update_data.sh
+++ content/ec2-spot-pricing/update_data.sh
@@ -2,4 +2,4 @@
 
 cd ~/repos/website/content/ec2-spot-pricing
 ~/ve_boto3/bin/python get_data.py
-aws s3 cp load_data.js s3://cpeccei-website-test/www/ec2-spot-pricing/
+aws s3 cp stats.json s3://cpeccei-website-test/www/ec2-spot-pricing/
