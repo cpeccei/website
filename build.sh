2afbd0801700889c583ffb8a86058cc449ea4ede
Christian Peccei <christian.peccei@gmail.com>
2021-04-03 22:39:14 -0700

    Add syntax highlighting

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 build () {
   cur_dir=$(dirname "$1")
   pandoc -s -o "$cur_dir/index.html" --template=template.html \
-    --no-highlight --css /base.css "$1"
+    --css /base.css "$1"
 }
 
 build content/body.md
